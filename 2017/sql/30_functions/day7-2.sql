e71ce2dfcf921d871a5c70f9b9fb92c8b989e28c
Feike Steenbergen <feike.steenbergen@adyen.com>
2017-12-07 11:30:50 +0100

    Day 7, part 2: Find the wrong weight

---
+++ 2017/sql/30_functions/day7-2.sql
@@ -0,0 +1,90 @@
+CREATE OR REPLACE FUNCTION day7_part2 (text default null, OUT name text, OUT current_weight bigint, OUT correct_weight bigint)
+LANGUAGE SQL AS $BODY$
+WITH RECURSIVE data AS (
+    SELECT
+        tokens[1] AS name,
+        tokens[2]::bigint AS weight,
+        nullif(items, '{""}') AS items
+    FROM
+        trim(coalesce($1, (SELECT input FROM input WHERE day=7))) AS t(input),
+        LATERAL regexp_split_to_table(input, E'\n') AS rstt(line),
+        LATERAL regexp_matches(line, '(\w+) \((\d+)\)(?: -> (.*))?') AS rm(tokens),
+        LATERAL regexp_split_to_array(coalesce(tokens[3], ''), ', ') AS rstt2(items)
+), root AS (
+    SELECT
+        name,
+        weight,
+        items,
+        ARRAY[name] AS path,
+        0 AS level,
+        items is null AS leaf
+    FROM
+        data
+    WHERE
+        name NOT IN (SELECT unnest(items) FROM data)
+    UNION ALL
+    SELECT
+        b.name,
+        b.weight,
+        b.items,
+        path||b.name,
+        level + 1,
+        b.items is null
+    FROM
+        root a
+    JOIN
+        data b ON (b.name = ANY (a.items))
+), test AS (
+    SELECT
+        a.name,
+        a.level,
+        sum(b.weight) + a.weight AS sum_weight,
+        a.path
+    FROM
+        root a
+    JOIN
+        root b ON (b.path @> a.path AND a.name != b.name)
+    GROUP BY
+        a.name,
+        a.level,
+        a.weight,
+        a.path
+), discrepancy AS (
+    SELECT
+        level,
+        path[:level] AS path
+    FROM
+        test
+    GROUP BY
+        level,
+        path[:level]
+    HAVING
+        min(sum_weight) != max(sum_weight)
+    ORDER BY
+        level DESC
+    LIMIT 1
+), mode (path, mode) AS (
+    SELECT
+        a.path,
+        mode() WITHIN GROUP (ORDER BY sum_weight)
+    FROM 
+        discrepancy a
+    JOIN
+        test b ON (a.path||b.name = b.path)
+    GROUP BY
+        a.path
+)
+SELECT
+    c.name,
+    weight::bigint AS current_weight,
+    (weight + (mode - sum_weight))::bigint AS correct_weight
+FROM
+    mode a
+JOIN
+    test b ON (a.path||b.name = b.path)
+JOIN
+    root c ON (b.path = c.path)
+WHERE
+    sum_weight != mode
+;
+$BODY$;
