20cdc0452729817f6251502e218aaebbf116b2f0
Feike Steenbergen <feike.steenbergen@adyen.com>
2017-12-11 10:51:21 +0100

    Day6, part 1

--- 2017/sql/30_functions/day6-1.sql
+++ 2017/sql/30_functions/day6-1.sql
@@ -2,19 +2,20 @@ DEALLOCATE day6_1;
 PREPARE day6_1 AS
 WITH RECURSIVE data(banks, size) AS (
     SELECT
-        banks::bigint[],
+        banks::smallint[],
         array_length(banks, 1)
     FROM
-        trim(coalesce(nullif($1, ''), (SELECT input FROM input WHERE day=6))) AS sub(t),
+        trim(coalesce($1, (SELECT input FROM input WHERE day=6))) AS sub(t),
         LATERAL regexp_split_to_array(t, '\s+') WITH ordinality AS rsta(banks)
 ) , iterate AS (
     SELECT
-        0::int AS round,
+        1::int AS round,
         banks,
         size,
         null::bigint AS max,
         null::bigint AS idx,
-        banks AS new_bank
+        banks AS new_bank,
+        ARRAY[]::text[] AS history
     FROM
         data
     UNION ALL
@@ -24,33 +25,38 @@ WITH RECURSIVE data(banks, size) AS (
         size,
         m.max,
         if.idx,
-        d.new_bank
+        d.new_bank,
+        history||d.new_bank::text
     FROM
         iterate AS i,
         LATERAL (SELECT max(unnest) FROM unnest(i.new_bank)) AS m(max),
         LATERAL (SELECT min(ordinality) FROM unnest(i.new_bank) WITH ORDINALITY WHERE unnest=m.max) AS if(idx),
         LATERAL (SELECT
                     array_agg(
-                        CASE ORDINALITY
+                        CASE ordinality
                             WHEN if.idx 
-                            THEN 0
-                            ELSE unnest
-                        END +
-                        (m.max/size) +
-                        CASE 
-                            WHEN if.idx <= (m.max%size)
-                            THEN 1
-                            ELSE 0
+                            THEN m.max/size
+                            ELSE 
+                                unnest +
+                                m.max/size +
+                                CASE 
+                                    -- We need to index the bank being emptied as 0, as
+                                    -- we distribute the items clockwise
+                                    WHEN (ordinality-if.idx+size)%size <= (m.max%size)
+                                    THEN 1
+                                    ELSE 0
+                                END     
                         END
-                    )
+                    )::smallint[]
                  FROM
                     unnest(i.new_bank) WITH ORDINALITY
                 ) as d(new_bank)
     WHERE
-        round < 200
+        NOT d.new_bank::text = ANY (history)
 )
 SELECT
-    *
+    max(round)
 FROM
     iterate
 ;
+EXECUTE day6_1('0 2 7 0');
