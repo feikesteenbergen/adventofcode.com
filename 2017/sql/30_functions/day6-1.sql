86d7f8ef8613ac2d2af6ac4600ee6636fbc15e9b
Feike Steenbergen <feike.steenbergen@adyen.com>
2017-12-11 08:48:54 +0100

    Intermediate for day 6

--- 2017/sql/30_functions/day6-1.sql
+++ 2017/sql/30_functions/day6-1.sql
@@ -1,39 +1,22 @@
+DEALLOCATE day6_1;
+PREPARE day6_1 AS
 WITH RECURSIVE data(banks, size) AS (
     SELECT
         banks::bigint[],
         array_length(banks, 1)
     FROM
-        trim(coalesce(null::text, (SELECT input FROM input WHERE day=6))) AS sub(t),
+        trim(coalesce(nullif($1, ''), (SELECT input FROM input WHERE day=6))) AS sub(t),
         LATERAL regexp_split_to_array(t, '\s+') WITH ordinality AS rsta(banks)
 ) , iterate AS (
     SELECT
         0::int AS round,
         banks,
         size,
-        max,
-        idx,
-        new_bank
+        null::bigint AS max,
+        null::bigint AS idx,
+        banks AS new_bank
     FROM
-        data,
-        LATERAL (SELECT max(unnest) FROM unnest(banks)) AS m(max),
-        LATERAL (SELECT min(ordinality) FROM unnest(banks) WITH ORDINALITY WHERE unnest=max) AS i(idx),
-        LATERAL (SELECT
-                    array_agg(
-                        CASE ORDINALITY
-                            WHEN idx 
-                            THEN 0
-                            ELSE unnest
-                        END +
-                        (max/size) +
-                        CASE 
-                            WHEN idx <= (max%size)
-                            THEN 1
-                            ELSE 0
-                        END
-                    )
-                 FROM
-                    unnest(banks) WITH ORDINALITY
-                ) as d(new_bank)
+        data
     UNION ALL
     SELECT
         round + 1,
